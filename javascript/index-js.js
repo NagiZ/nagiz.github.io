c4bee04284112661ea768e1b447f0fd849442cfb
NagiZ <863984461@qq.com>
2017-07-26 20:50:34 +0800

    portfolio

---
+++ javascript/index-js.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	// Stuff to do as soon as the DOM is ready;
+
+	var	$nav_ul = $("#nav-ul").children();
+	$(window).scroll(function(){
+		var wHeight = 0.5*$(window).height();
+		var demosTop = $("#demos").offset().top - wHeight,
+			contactTop = $("#contact").offset().top - wHeight,
+			top = $(document).scrollTop();
+		if (top>contactTop) {
+			exchangeClass($nav_ul, 2);
+		}else if(top>demosTop){
+			exchangeClass($nav_ul, 1);
+		}else{
+			exchangeClass($nav_ul, 0);
+		}
+
+	})
+
+});
+
+function exchangeClass($eleArr, index){
+	for (var i = 0; i < $eleArr.length; i++) {
+		$eleArr.eq(i).removeClass('active');
+	}
+	$eleArr.eq(index).addClass('active');
+}
